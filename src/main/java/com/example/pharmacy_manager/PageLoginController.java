03c5d5296c8a7505cbddd09d9d23c97d5bbb1b34
Ism4Xiil <ismailmahmoud658@gmail.com>
2023-01-03 10:14:18 +0100

    7th commit

---
+++ src/main/java/com/example/pharmacy_manager/PageLoginController.java
@@ -0,0 +1,65 @@
+package com.example.pharmacy_manager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javax.swing.*;
+
+import static com.example.pharmacy_manager.PageLogin.jobAccount;
+
+
+public class PageLoginController implements Initializable {
+
+
+    @FXML
+    private Label errorLabel = new Label();
+
+    @FXML
+    private Label usernameLab = new Label("Username");
+
+    @FXML
+    private TextField edtUsername = new TextField();
+
+    @FXML
+    private Label passwordLab = new Label("Password");
+    @FXML
+    private TextField edtPassword = new TextField();
+
+    @FXML
+    private Button loginBtn = new Button("Login");
+
+
+    //ISMAIL HNA ZID kemel DECLARI LES VIEWS LI TES7A9HOM,
+    //.
+    //.
+    //.
+    //.
+    //.
+
+
+
+
+
+
+
+    @FXML
+    public void loginBtn(ActionEvent actionEvent) {
+        if (PageLogin.login(edtUsername.getText(), edtPassword.getText())) {
+            dispose();
+            PageDashboard  dbPage = new PageDashboard();
+            dbPage.setVisible(true);
+            if (jobAccount == "vendor") {
+
+            }
+        } else {
+            errorLabel.setText("Username or password entered is wrong");
+            edtUsername.setText(" ");
+            edtPassword.setText(" ");
+        }
+
+    }
+}
+
